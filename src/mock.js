262fa907360afdcb1b7c143cb19cba51d3659664
aoliveira <aoliveira@keener.io>
2022-04-27 20:03:11 -0300

    Cria função funcional para arrastar e soltar

--- src/mock.js
+++ src/mock.js
@@ -1,62 +1,72 @@
 export const data = [
-    {
-        state: 0,
-        text: 'Tarefa Aqui', id: 0
-    },
-    {
-        state: 0,
-        text: 'Personalizar as telas de erro do template metronic', id: 1
-    },
-    {
-        state: 0,
-        text: 'Personalizar as telas de erro do template metronic', id: 2
-    },
-    {
-        state: 0,
-        text: 'Personalizar as telas de erro do template metronic', id: 3
-    },
-    {
-        state: 0,
-        text: 'Personalizar as telas de erro do template metronic', id: 4
-    },
-    {
-        state: 1,
-        text: 'Personalizar as telas de erro do template metronic', id: 5
-    },
-    {
-        state: 1,
-        text: 'Personalizar as telas de erro do template metronic', id: 6
-    },
-    {
-        state: 1,
-        text: 'Personalizar as telas de erro do template metronic', id: 7
-    },
-    {
-        state: 1,
-        text: 'Personalizar as telas de erro do template metronic', id: 8
-    },
-    {
-        state: 1,
-        text: 'Personalizar as telas de erro do template metronic', id: 9
-    },
-    {
-        state: 2,
-        text: 'Personalizar as telas de erro do template metronic', id: 10
-    },
-    {
-        state: 2,
-        text: 'Personalizar as telas de erro do template metronic', id: 11
-    },
-    {
-        state: 2,
-        text: 'Personalizar as telas de erro do template metronic', id: 12
-    },
-    {
-        state: 2,
-        text: 'Personalizar as telas de erro do template metronic', id: 13
-    },
-    {
-        state: 2,
-        text: 'Personalizar as telas de erro do template metronic', id: 14
-    },
-]
+  {
+    state: 0,
+    text: "Tarefa Aqui",
+    id: 0,
+  },
+  {
+    state: 0,
+    text: "Mais uma tarefa",
+    id: 1,
+  },
+  {
+    state: 0,
+    text: "Outra aqui talvez",
+    id: 2,
+  },
+  {
+    state: 0,
+    text: "Eita, mais um cara",
+    id: 3,
+  },
+  {
+    state: 0,
+    text: "Tarefinha de leve",
+    id: 4,
+  },
+  {
+    state: 1,
+    text: "Hello, sou mais uma tarefa",
+    id: 5,
+  },
+  {
+    state: 1,
+    text: "Tarefa que não acaba mais",
+    id: 6,
+  },
+  {
+    state: 1,
+    text: "Tarefa y tarefa",
+    id: 7,
+  },
+  {
+    state: 1,
+    text: "Sabe aquela tarefa?",
+    id: 8,
+  },
+  {
+    state: 1,
+    text: "Então, ainda não foi realizada",
+    id: 9,
+  },
+  {
+    state: 2,
+    text: "Com certeza essa tarefa aqui vai rolar",
+    id: 10,
+  },
+  {
+    state: 2,
+    text: "Tarefa super interessante",
+    id: 11,
+  },
+  {
+    state: 2,
+    text: "Talvez eu não precise fazer isso",
+    id: 12,
+  },
+  {
+    state: 2,
+    text: "Amanhã eu faço",
+    id: 13,
+  },
+];
