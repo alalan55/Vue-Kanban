1dc3780e9023a94bed1bacde8fde2df985fa9598
aoliveira <aoliveira@keener.io>
2022-04-27 14:33:06 -0300

    Inicia a construção da página home

---
+++ src/components/organisms/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <span>Header</span>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  border: 1px solid blue;
+}
+</style>
---
+++ src/components/templates/HomeTemplate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home__template">
+    <HeaderComponent />
+    <div class="__content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/organisms/HeaderComponent.vue";
+export default {
+  components: {
+    HeaderComponent,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home__template {
+  width: 100%;
+ 
+  .__content{
+      height: calc(100vh - 70px);
+  }
+}
+</style>
